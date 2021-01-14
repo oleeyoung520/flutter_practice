e3efcb73c4f7174afee70423ece2e2ea3c3c0748
oleeyoung520 <2015147520@yonsei.ac.kr>
2021-01-14 17:16:49 +0900

    add flutter_application_1

---
+++ flutter_application_1/lib/LoginFormRoute.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginFormRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center (
+        child: Text('LoginFormRoute')
+      )
+    );
+  }
+}
---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'LoginFormRoute.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: SplashScreen(),
+        routes: <String, WidgetBuilder> {
+          '/LoginFormRoute': (BuildContext context) => LoginFormRoute()
+    }
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 1);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/LoginFormRoute');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: Text('hello world'),
+      ),
+    );
+  }
+}
